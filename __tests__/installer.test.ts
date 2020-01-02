a46f9f3e8cbb75f483a48c70a0be6062e9bfca24
komei <33626923+er28-0652@users.noreply.github.com>
2020-01-03 01:36:07 +0900

    lint

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -1,20 +1,19 @@
-import io = require('@actions/io');
-import fs = require('fs');
-import path = require('path');
-import child_process = require('child_process');
+import io = require("@actions/io");
+import fs = require("fs");
+import path = require("path");
+import child_process = require("child_process");
 
-const toolDir = path.join(__dirname, 'runner', 'tools');
-const tempDir = path.join(__dirname, 'runner', 'temp');
+const toolDir = path.join(__dirname, "runner", "tools");
+const tempDir = path.join(__dirname, "runner", "temp");
 
-process.env['RUNNER_TOOL_CACHE'] = toolDir;
-process.env['RUNNER_TEMP'] = tempDir;
-import * as installer from '../src/installer';
+process.env["RUNNER_TOOL_CACHE"] = toolDir;
+process.env["RUNNER_TEMP"] = tempDir;
+import * as installer from "../src/installer";
 
-const ghidraFilePath = path.join(tempDir, 'ghidra.zip');;
-const ghidraUrl = 'https://ghidra-sre.org/ghidra_9.1.1_PUBLIC_20191218.zip';
+const ghidraFilePath = path.join(tempDir, "ghidra.zip");
+const ghidraUrl = "https://ghidra-sre.org/ghidra_9.1.1_PUBLIC_20191218.zip";
 
-
-describe('installer tests', () => {
+describe("installer tests", () => {
   beforeAll(async () => {
     await io.rmRF(toolDir);
     await io.rmRF(tempDir);
@@ -22,10 +21,10 @@ describe('installer tests', () => {
       // Download java
       await io.mkdirP(tempDir);
 
-      console.log('Downloading java');
+      console.log("Downloading java");
       child_process.execSync(`curl "${ghidraUrl}" > "${ghidraFilePath}"`);
       // Write complete file so we know it was successful
-      fs.writeFileSync(`${ghidraFilePath}.complete`, 'content');
+      fs.writeFileSync(`${ghidraFilePath}.complete`, "content");
     }
   }, 300000);
 
@@ -34,33 +33,30 @@ describe('installer tests', () => {
       await io.rmRF(toolDir);
       await io.rmRF(tempDir);
     } catch {
-      console.log('Failed to remove test directories');
+      console.log("Failed to remove test directories");
     }
   }, 100000);
-  
-  
-  it('Installs version of Ghidra if no matching version is installed', async () => {
-    await installer.installGhidra('9.1.1');
-    const ghidraDir = path.join(toolDir, 'ghidra', '9.1.1');
+
+  it("Installs version of Ghidra if no matching version is installed", async () => {
+    await installer.installGhidra("9.1.1");
+    const ghidraDir = path.join(toolDir, "ghidra", "9.1.1");
 
     expect(fs.existsSync(`${ghidraDir}.complete`)).toBe(true);
-    expect(fs.existsSync(path.join(ghidraDir, 'ghidraRun'))).toBe(true);
-    }, 100000);
+    expect(fs.existsSync(path.join(ghidraDir, "ghidraRun"))).toBe(true);
+  }, 100000);
 
+  it("Throws if invalid directory to Ghidra", async () => {
+    let thrown = false;
+    try {
+      await installer.installGhidra("tesuyatesuya");
+    } catch {
+      thrown = true;
+    }
+    expect(thrown).toBe(true);
+  });
 
-    it('Throws if invalid directory to Ghidra', async () => {
-      let thrown = false;
-      try {
-        await installer.installGhidra('tesuyatesuya');
-      } catch {
-        thrown = true;
-      }
-      expect(thrown).toBe(true);
-    });
-
-
-    it('Get GHIDRA_INSTALL_DIR is defined', async () => {
-      await installer.installGhidra('9.1.1');
-      expect(process.env['GHIDRA_INSTALL_DIR']).toBe(true);
-    });
+  it("Get GHIDRA_INSTALL_DIR is defined", async () => {
+    await installer.installGhidra("9.1.1");
+    expect(process.env["GHIDRA_INSTALL_DIR"]).toBe(true);
+  });
 });
--- src/installer.ts
+++ src/installer.ts
@@ -1,76 +1,68 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as core from '@actions/core';
-import * as tc from '@actions/tool-cache';
-import * as httpm from 'typed-rest-client/HttpClient';
+import * as fs from "fs";
+import * as path from "path";
+import * as core from "@actions/core";
+import * as tc from "@actions/tool-cache";
+import * as httpm from "typed-rest-client/HttpClient";
 
-let tempDirectory = process.env['RUNNER_TEMP'] || '';
-const IS_WINDOWS = process.platform === 'win32';
+let tempDirectory = process.env["RUNNER_TEMP"] || "";
+const IS_WINDOWS = process.platform === "win32";
 
 if (!tempDirectory) {
   let baseLocation;
   if (IS_WINDOWS) {
     // On windows use the USERPROFILE env variable
-    baseLocation = process.env['USERPROFILE'] || 'C:\\';
+    baseLocation = process.env["USERPROFILE"] || "C:\\";
   } else {
-    if (process.platform === 'darwin') {
-      baseLocation = '/Users';
+    if (process.platform === "darwin") {
+      baseLocation = "/Users";
     } else {
-      baseLocation = '/home';
+      baseLocation = "/home";
     }
   }
-  tempDirectory = path.join(baseLocation, 'actions', 'temp');
+  tempDirectory = path.join(baseLocation, "actions", "temp");
 }
 
-const GHIDRA_BASE_URL = 'https://ghidra-sre.org/'
-const http: httpm.HttpClient = new httpm.HttpClient('setup-ghidra');
-
+const GHIDRA_BASE_URL = "https://ghidra-sre.org/";
+const http: httpm.HttpClient = new httpm.HttpClient("setup-ghidra");
 
 interface Dict {
   [key: string]: string;
 }
 
-
 async function getGhidraVersionInfo(): Promise<Dict> {
   // get release page
-  let topPageHTML = await (
-    await http.get(GHIDRA_BASE_URL)
-  ).readBody();
-  let releaseNoteURL = topPageHTML.match(/releaseNotes.*\.html/) || '';
+  let topPageHTML = await (await http.get(GHIDRA_BASE_URL)).readBody();
+  let releaseNoteURL = topPageHTML.match(/releaseNotes.*\.html/) || "";
 
   let releaseNoteHTML = await (
     await http.get(GHIDRA_BASE_URL + releaseNoteURL)
   ).readBody();
-  
+
   // parse Ghidra version numbre and archive name
   const ptn = /<td>(\d\.\d\.\d)<\/td>\r\n.*<a href=\"(ghidra_.*?_PUBLIC_\d{8}\.zip)\">/gi;
   let m;
   let versionInfo: Dict = {};
-  while ((m = ptn.exec(releaseNoteHTML)) !== null){
+  while ((m = ptn.exec(releaseNoteHTML)) !== null) {
     versionInfo[m[1]] = m[2];
   }
   return versionInfo;
 }
 
-
 async function getLatestGhidraVersionInfo(): Promise<Dict> {
-  let topPageHTML = await (
-    await http.get(GHIDRA_BASE_URL)
-  ).readBody();
+  let topPageHTML = await (await http.get(GHIDRA_BASE_URL)).readBody();
 
   let m = topPageHTML.match(/href=\"(ghidra_.*?_PUBLIC_\d{8}\.zip)\"/);
   if (m === null) {
-    throw new Error('Ghidra Archive List was not found.');
-  }else{
+    throw new Error("Ghidra Archive List was not found.");
+  } else {
     let latestVersionZip = m[1];
     let latestVersionInfo: Dict = {};
 
-    let version = latestVersionZip.split('_')[1];
+    let version = latestVersionZip.split("_")[1];
     latestVersionInfo[version] = latestVersionZip;
     return latestVersionInfo;
   }
 }
-
 
 async function extractFiles(
   file: string,
@@ -91,34 +83,29 @@ async function extractGhidraArchive(src: string, dst: string): Promise<string> {
   const stats = fs.statSync(ghidraArchivePath);
   if (stats.isFile()) {
     await extractFiles(src, dst);
-    const ghidraDirectory = path.join(
-      dst,
-      fs.readdirSync(dst)[0]
-    );
+    const ghidraDirectory = path.join(dst, fs.readdirSync(dst)[0]);
     return ghidraDirectory;
   } else {
     throw new Error(`${ghidraArchivePath} is not a file`);
   }
 }
 
-export async function installGhidra(version: string = ''): Promise<void>{
-
-  let toolPath = tc.find('ghidra', version);
+export async function installGhidra(version: string = ""): Promise<void> {
+  let toolPath = tc.find("ghidra", version);
 
   if (toolPath) {
     core.debug(`Tool found in cache ${toolPath}`);
-  }else{
-    let ghidraVersionInfo:any = {version: '', archive: ''};
+  } else {
+    let ghidraVersionInfo: any = { version: "", archive: "" };
 
-    if (version === '') {
+    if (version === "") {
       let info = await getLatestGhidraVersionInfo();
-      Object.entries(info)
-      .map(([_version, ghidraZipName]) => {
+      Object.entries(info).map(([_version, ghidraZipName]) => {
         ghidraVersionInfo.version = version;
         ghidraVersionInfo.archive = ghidraZipName;
       });
-    }else{
-      let info  = await getGhidraVersionInfo();
+    } else {
+      let info = await getGhidraVersionInfo();
       if (info[version] === undefined) {
         const err: Error = new Error(`[ERROR] ${version} is not found.`);
         throw err;
@@ -126,21 +113,21 @@ export async function installGhidra(version: string = ''): Promise<void>{
       ghidraVersionInfo.version = version;
       ghidraVersionInfo.archive = info[version];
     }
-  
-    console.log(`Version: ${ghidraVersionInfo.version}, Archive: ${ghidraVersionInfo.archive}`);
-    let savedPath = await tc.downloadTool(GHIDRA_BASE_URL + ghidraVersionInfo.archive);
-    
+
+    console.log(
+      `Version: ${ghidraVersionInfo.version}, Archive: ${ghidraVersionInfo.archive}`
+    );
+    let savedPath = await tc.downloadTool(
+      GHIDRA_BASE_URL + ghidraVersionInfo.archive
+    );
+
     let tempDir: string = path.join(
       tempDirectory,
-      'temp_' + Math.floor(Math.random() * 2000000000)
+      "temp_" + Math.floor(Math.random() * 2000000000)
     );
 
     let ghidraInstallDir = await extractGhidraArchive(savedPath, tempDir);
-    toolPath = await tc.cacheDir(
-      ghidraInstallDir,
-      'ghidra',
-      version,
-    );
+    toolPath = await tc.cacheDir(ghidraInstallDir, "ghidra", version);
   }
-  core.exportVariable('GHIDRA_INSTALL_DIR', toolPath);
+  core.exportVariable("GHIDRA_INSTALL_DIR", toolPath);
 }
